d1ddcddb4656ebedce97948fe3d147b504a3d703
Ted Dunstone <dunstt@aifdr.org>
2011-01-27 10:15:40 +0700

    GeoExt geoserver test

---
+++ source/riab_basic/riab_map.js
@@ -0,0 +1,244 @@
+/**
+ * Copyright (c) 2008-2010 The Open Source Geospatial Foundation
+ * 
+ * Published under the BSD license.
+ * See http://svn.geoext.org/core/trunk/geoext/license.txt for the full text
+ * of the license.
+ */
+
+/* Basic RIAB map view for layers
+*/
+
+var mapPanel, tree;
+Ext.onReady(function() {
+    // create a map panel
+
+    var ctrl, toolbarItems = [], action, actions = {};
+
+    var map = new OpenLayers.Map({allOverlays: false})
+
+    var vector = new OpenLayers.Layer.Vector("vector");
+ 
+   // ZoomToMaxExtent control, a "button" control
+    action = new GeoExt.Action({
+        control: new OpenLayers.Control.ZoomToMaxExtent(),
+        map: map,
+        text: "calculate",
+        tooltip: "calculate risk"
+    });
+    actions["max_extent"] = action;
+    toolbarItems.push(action);
+    toolbarItems.push("-");
+
+    action = new GeoExt.Action({
+        text: "select region",
+        control: new OpenLayers.Control.DrawFeature(
+            vector, OpenLayers.Handler.Polygon
+        ),
+        map: map,
+        // button options
+        toggleGroup: "draw",
+        allowDepress: false,
+        tooltip: "draw polygon",
+      // check item options
+        group: "draw"
+    });             
+    actions["draw_poly"] = action;
+    toolbarItems.push(action);
+
+
+    //toolbarItems.push({
+    //    text: "menu",
+    //    menu: new Ext.menu.Menu({
+    //        items: [
+                // ZoomToMaxExtent
+    //            actions["max_extent"]
+//                new Ext.menu.CheckItem(actions["draw_poly"]),
+                // Nav
+                //new Ext.menu.CheckItem(actions["nav"]),
+      //      ]
+      //  })
+   // });
+
+
+    mapPanel = new GeoExt.MapPanel({
+        border: true,
+        region: "center",
+        // we do not want all overlays, to try the OverlayLayerContainer
+        map: map,
+        tbar: toolbarItems,
+        extent: "105.3000035, -8.3749995,110.29,-5.566",
+        layers: [
+           new OpenLayers.Layer.WMS("Global",
+                "http://maps.opengeo.org/geowebcache/service/wms", {
+                    layers: "bluemarble"
+                }, {
+                    buffer: 0
+                }
+            ),
+
+            // create a group layer (with several layers in the "layers" param)
+            // to show how the LayerParamLoader works
+            new OpenLayers.Layer.WMS("Group Layer",
+                "http://127.0.0.1:8080/geoserver/wms", {
+                    layers: [
+                        "futnuh:mmi_lembang_68",
+                        "futnuh:bridge_S68_WestJava" ,
+			"futnuh:fatality_padang_1_calculated_by_riab",
+                    ],
+                    transparent: true,
+                    format: "image/gif"
+                }, {
+                    isBaseLayer: false,
+                    buffer: 0,
+                    // exclude this layer from layer container nodes
+                    displayInLayerSwitcher: false,
+                    visibility: false
+                }
+            )
+        ]
+    });
+
+
+
+    // create our own layer node UI class, using the TreeNodeUIEventMixin
+    var LayerNodeUI = Ext.extend(GeoExt.tree.LayerNodeUI, new GeoExt.tree.TreeNodeUIEventMixin());
+        
+    // using OpenLayers.Format.JSON to create a nice formatted string of the
+    // configuration for editing it in the UI
+    var treeConfig = new OpenLayers.Format.JSON().write([{
+        nodeType: "gx_baselayercontainer"
+    }, {
+        nodeType: "gx_overlaylayercontainer",
+        expanded: true,
+        // render the nodes inside this container with a radio button,
+        // and assign them the group "foo".
+        loader: {
+            baseAttrs: {
+                radioGroup: "foo",
+                uiProvider: "layernodeui"
+            }
+        }
+    }, {
+        nodeType: "gx_layer",
+        layer: "Group Layer",
+        isLeaf: false,
+        // create subnodes for the layers in the LAYERS param. If we assign
+        // a loader to a LayerNode and do not provide a loader class, a
+        // LayerParamLoader will be assumed.
+        loader: {
+            param: "LAYERS"
+        }
+    }], true);
+
+    // create the tree with the configuration from above
+    tree = new Ext.tree.TreePanel({
+        border: true,
+        region: "west",
+        title: "Layers",
+        width: 200,
+        split: true,
+        collapsible: true,
+        collapseMode: "mini",
+        autoScroll: true,
+        plugins: [
+            new GeoExt.plugins.TreeNodeRadioButton({
+                listeners: {
+                    "radiochange": function(node) {
+                        alert(node.text + " is now the active layer.");
+                    }
+                }
+            })
+        ],
+        loader: new Ext.tree.TreeLoader({
+            // applyLoader has to be set to false to not interfer with loaders
+            // of nodes further down the tree hierarchy
+            applyLoader: false,
+            uiProviders: {
+                "layernodeui": LayerNodeUI
+            }
+        }),
+        root: {
+            nodeType: "async",
+            // the children property of an Ext.tree.AsyncTreeNode is used to
+            // provide an initial set of layer nodes. We use the treeConfig
+            // from above, that we created with OpenLayers.Format.JSON.write.
+            children: Ext.decode(treeConfig)
+        },
+        listeners: {
+            "radiochange": function(node){
+                alert(node.layer.name + " is now the the active layer.");
+            }
+        },
+        rootVisible: false,
+        lines: false,
+        bbar: [{
+            text: "Show/Edit Tree Config",
+            handler: function() {
+                treeConfigWin.show();
+                Ext.getCmp("treeconfig").setValue(treeConfig);
+            }},
+            {
+            text:"Calculate",
+            }
+        ]
+    });
+
+    // dialog for editing the tree configuration
+    var treeConfigWin = new Ext.Window({
+        layout: "fit",
+        hideBorders: true,
+        closeAction: "hide",
+        width: 300,
+        height: 400,
+        title: "Tree Configuration",
+        items: [{
+            xtype: "form",
+            layout: "fit",
+            items: [{
+                id: "treeconfig",
+                xtype: "textarea"
+            }],
+            buttons: [{
+                text: "Save",
+                handler: function() {
+                    var value = Ext.getCmp("treeconfig").getValue()
+                    try {
+                        var root = tree.getRootNode();
+                        root.attributes.children = Ext.decode(value);
+                        tree.getLoader().load(root);
+                    } catch(e) {
+                        alert("Invalid JSON");
+                        return;
+                    }
+                    treeConfig = value;
+                    treeConfigWin.hide();
+                }
+            }, {
+                text: "Cancel",
+                handler: function() {
+                    treeConfigWin.hide();
+                }
+            }]
+        }]
+    });
+    
+    new Ext.Viewport({
+        layout: "fit",
+        hideBorders: true,
+        items: {
+            layout: "border",
+            deferredRender: false,
+            items: [mapPanel, tree, {
+                contentEl: "desc",
+                region: "east",
+                bodyStyle: {"padding": "5px"},
+                collapsible: true,
+                collapseMode: "mini",
+                split: true,
+                width: 200,
+                title: "Description"
+            }]
+        }
+    });
+});
